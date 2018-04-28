cd42aa121ec8b1db195e5e3bcc84d2fd26fcd9a2
Henrique Filho <hbaf71@gmail.com>
2018-04-28 13:33:18 -0300

    Javadjustments

---
+++ Java/PortuJava/Lista01/src/lista01/Exercicio01.java
@@ -0,0 +1,12 @@
+package lista01;
+/**
+ * 1. Faça um algoritmo que imprima o seu nome e sua idade.
+ * @author Henrique Almeida
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        String nome = "Henrique";
+        int idade = 18;
+        System.out.println("Meu nome é "+nome+" e eu tenho "+idade+" anos.");
+    }
+}
