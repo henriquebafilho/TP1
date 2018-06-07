66fbf30b9fcb8b34dc0e5c5a6b06c9515843464d
Henrique Filho <hbaf71@gmail.com>
2018-06-07 12:27:26 -0300

    Lista18Exercicio02c

---
+++ Java/src/Lista18Exercicio02c.java
@@ -0,0 +1,15 @@
+
+/*c) Escreva um programa que elimine os espaços em branco duplos da sinopse de um filme.*/
+import java.util.*;
+
+public class Lista18Exercicio02c {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String sinopse;
+		
+		System.out.println("Insira a sinopse do filme:");
+		sinopse = entrada.nextLine();
+		sinopse = sinopse.replaceAll(" ", "");
+		System.out.println(sinopse);
+	}
+}
