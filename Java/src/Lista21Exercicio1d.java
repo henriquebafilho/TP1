019a39b8bc4cb46a5dd0fd9123516523641bc0f0
Henrique Filho <hbaf71@gmail.com>
2018-06-21 12:27:54 -0300

    Lista21Exercicio1d

---
+++ Java/src/Lista21Exercicio1d.java
@@ -0,0 +1,28 @@
+
+/*1) Para as sequencias a seguir, escreva uma função recursiva que seja capaz de retornar
+o elemento desejado da sequencia. A função main é responsável pela iteração com o
+usuário, que indicará qual elemento da sequencia ele deseja visualizar.
+d) 1,3,9,27,81,...*/
+import java.util.*;
+
+public class Lista21Exercicio1d {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int posicao;
+
+		System.out.println("Insira a posição do elemento que você deseja encontrar:");
+		posicao = entrada.nextInt();
+		System.out.println("O elemento na posição "+posicao+" é "+procurar(posicao));
+	}
+
+	public static int procurar(int p) {
+		int ultimo;
+		
+		if(p == 1) {
+			ultimo = 1;
+		} else {
+			ultimo = 3*procurar(p-1);
+		}
+		return ultimo;
+	}
+}
