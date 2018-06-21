1a3275e559a0dac3b0ac7eaaa59c0c5dcc6a701a
Henrique Filho <hbaf71@gmail.com>
2018-06-21 12:10:51 -0300

    Lista21Exercicio1a

--- Java/src/Lista21Exercicio1a.java
+++ Java/src/Lista21Exercicio1a.java
@@ -14,22 +14,17 @@ public class Lista21Exercicio1a {
 		System.out.println("Insira a posição do elemento que você deseja encontrar:");
 		posicao = entrada.nextInt();
 		int[] numeros = new int[posicao];
-		procurar(posicao);
+		System.out.println("O elemento na posição "+posicao+" é "+procurar(posicao));
 	}
 
 	public static int procurar(int p) {
-		if(p == 0) {
-			numeros[0] = 2;
-			
-		} else {
-			System.out.println(p-1);
-			numeros[p-1] = 2*procurar(p-1);
-		}
-		//Mostrando os números
-		for (int i = 0; i < numeros.length; i++) {
-			System.out.println(numeros[i]);
-		}
+		int ultimo;
 		
-		return numeros[p-1];
+		if(p == 1) {
+			ultimo = 2;
+		} else {
+			ultimo = 2*procurar(p-1);
+		}
+		return ultimo;
 	}
 }
