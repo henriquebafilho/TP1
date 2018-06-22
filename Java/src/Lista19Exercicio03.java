ae243f88e0f3fc4d4fe624d8627c0d7e17bbcc35
Henrique Filho <hbaf71@gmail.com>
2018-06-21 23:50:34 -0300

    Lista19Exercicio03

--- Java/src/Lista19Exercicio03.java
+++ Java/src/Lista19Exercicio03.java
@@ -26,25 +26,27 @@ public class Lista19Exercicio03 {
 	public static void upper(String n) {
 		String[] palavras = n.split(" ");
 		ArrayList<String> cortado = new ArrayList();
-		
-		//inserindo os nomes numa arraylist
-		for(int i = 0; i < palavras.length; i++) {
+
+		// inserindo os nomes numa arraylist
+		for (int i = 0; i < palavras.length; i++) {
 			cortado.add(palavras[i]);
 		}
-		//removento conectivos
-		for(int i = 0; i < palavras.length; i++) {
-			if(cortado.get(i).contains("de") && cortado.get(i).length()==2) {
+		// removento conectivos
+		for (int i = 0; i < cortado.size(); i++) {
+			if ((cortado.get(i).contains("de") && cortado.get(i).length() == 2)
+					|| (cortado.get(i).contains("e") && cortado.get(i).length() == 1)
+					|| (cortado.get(i).contains("do") && cortado.get(i).length() == 2)
+					|| (cortado.get(i).contains("da") && cortado.get(i).length() == 2)
+					|| (cortado.get(i).contains("dos") && cortado.get(i).length() == 3)
+					|| (cortado.get(i).contains("das") && cortado.get(i).length() == 3)
+					|| (cortado.get(i).contains("di") && cortado.get(i).length() == 2)
+					|| (cortado.get(i).contains("du") && cortado.get(i).length() == 2)) {
 				cortado.remove(i);
-				System.out.println(cortado.get(i));
 			}
 		}
-		//colocando iniciais maiúsculas
-		for(int i = 0; i < palavras.length; i++) {
-			cortado.get(i).toUpperCase().charAt(0);
-		}
-		//Mostrando iniciais
-		for(int i = 0; i < palavras.length; i++) {
-			System.out.print(cortado.get(i));
+		// Mostrando iniciais
+		for (int i = 0; i < cortado.size(); i++) {
+			System.out.print(cortado.get(i).toUpperCase().charAt(0));
 		}
 	}
 }
