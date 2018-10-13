7fa5b22d96fe7d2b22e3504bce5ac84c2f94c2be
Henrique Filho <hbaf71@gmail.com>
2018-10-13 01:57:49 -0300

    changes

---
+++ Java/src/pascal.java
@@ -0,0 +1,20 @@
+
+public class pascal {
+	public void Pascal(int n) {
+		int nfilas = n;
+		int[] a = new int[1];
+		for (int i = 1; i <= nfilas; i++) {
+			int[] x = new int[i];
+			for (int j = 0; j < i; j++) {
+				if (j == 0 || j == (i - 1)) {
+					x[j] = 1;
+				} else {
+					x[j] = a[j] + a[j - 1];
+				}
+				System.out.print(x[j] + " ");
+			}
+			a = x;
+			System.out.println();
+		}
+	}
+}
